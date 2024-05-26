06726e21da877c29b8248908e1189408a485c13d
Dan Koskei <qimtaid@gmail.com>
2024-05-26 21:56:57 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matches = []
+        for word in words:
+            if sorted(self.word) == sorted(word):
+                matches.append(word)
+        return matches
